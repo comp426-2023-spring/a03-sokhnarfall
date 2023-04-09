c60a72779c14c7dda741299d7a1bc40333f715b3
Sokhna Fall <84427104+sokhnarfall@users.noreply.github.com>
2023-04-09 19:29:53 -0400

    Update rps-cli.js

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -34,7 +34,7 @@ if (args.r || args.rules) {
 let action = args._[0];
 
 try {
-    console.log(JSON.stringify(rps_function(action)));
+    console.log(JSON.stringify(rps(action)));
     
 } catch (e){
     console.log(helpMessage);
