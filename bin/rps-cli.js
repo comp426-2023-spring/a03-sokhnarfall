a0c1c328b11a918d70a675066f8743453b9eb0b0
Sokhna Fall <84427104+sokhnarfall@users.noreply.github.com>
2023-04-09 22:16:01 -0400

    Update rps-cli.js

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -1,15 +1,10 @@
 #!/usr/bin/env node
-import minimist from 'minimist'
-import { rps } from 'node-rpsls'
 
+import minimist from "minimist";
+import { rps } from "../lib/rpsls.js";
 
 const args = minimist(process.argv.slice(2));
-const rules = `Rules for Rock Paper Scissors:
-- Scissors CUTS Paper
-- Paper COVERS Rock
-- Rock CRUSHES Scissors`;
-
-const helpMessage = `Usage: node-rps [SHOT]
+const helpMessage= `Usage: node-rps [SHOT]
 Play Rock Paper Scissors (RPS)
   -h, --help      display this help message and exit
   -r, --rules     display the rules and exit
@@ -19,28 +14,26 @@ Examples:
   node-rps rock   Return JSON with results for RPS played against a simulated opponent.
                   e.g {"player":"rock","opponent":"scissors","result":"win"}`;
 
+const rules = `Rules for Rock Paper Scissors:
+- Scissors CUTS Paper
+- Paper COVERS Rock
+- Rock CRUSHES Scissors`;
 
-if (args.h || args.help) {
-  console.log(helpMessage);
-  process.exit(0);
+if (args.help || args.h) {
+    console.log(helpMessage);
+    process.exit(0);
 }
-
-
-if (args.r || args.rules) {
-  console.log(rules);
-  process.exit(0);
+if (args.rules || args.r) {
+    console.log(rules);
+    process.exit(0);
 }
-
 let action = args._[0];
 
 try {
     console.log(JSON.stringify(rps(action)));
-} 
-catch (error){
+    
+} catch (error){
     console.log(helpMessage);
     console.log(rules);
     process.exit(0);
 }
-
-
-
